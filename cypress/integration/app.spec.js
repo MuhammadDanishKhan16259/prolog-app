db54ddbb1271be01bbbdc728a657b82acb689b37
MuhammadDanishKhan16259 <muhammaddanishkhan1999@gmail.com>
2024-02-20 13:25:05 +0500

    Adds Prettier format script

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -2,6 +2,6 @@ describe("Home Page", () => {
   it("Test hi", () => {
     cy.visit("http://localhost:3000/");
     // cy.get("title").contains("Welcome To Next.Js!");
-    cy.get("h1").contains("Welcome to Prolong APP");
+    cy.get("h1").contains("Welcome to Prolong APP!");
   });
 });
